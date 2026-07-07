a65ead8ecec4373ef1e51def7ac6f4e507f3a266
MT Rashid <mt.rashid.dev@gmail.com>
2026-07-07 08:31:01 +0600

    add model User

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
-const express = require("express");
+import express from "express";
+
+import authRoutes from "./routes/auth.route.js";
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send('Hello World!');
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
   console.log(`Backend app listening on port ${port}`);
